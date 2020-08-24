a649b2c1c9352e6f78e5bd383208a5437845863e
Chad Furman <chad@chadfurman.com>
2020-08-23 21:35:33 -0400

    chore: formatting and fixing a test

--- nmap2cherry.py
+++ nmap2cherry.py
@@ -1,167 +1,273 @@
 import xml.etree.ElementTree as ET
+import sys
+
 
 def create_raw_nmap_output_root_from_file(filename):
-	'''
-	load the nmap.xml file into an element tree
-	'''
-	tree = ET.parse(filename)
-	return tree.getroot()
+    '''
+    load the nmap.xml file into an element tree
+    '''
+    tree = ET.parse(filename)
+    return tree.getroot()
 
-def add_rich_text_to_node(node, content=None):
-	'''
-	Add a <rich_text> subnode with content to the given parent node
-	returns the new rich text node
-	'''
-	rich_text = ET.SubElement(node,'rich_text')		# make the node
-	if (content == None):
-		content = ''
-	rich_text.text = str(content)					# set the inner text
-	return rich_text								# return the new rich text node
+
+def add_rich_text_node_to_node(
+        node,
+        name,
+        content=None,
+        prog_lang='custom_colors',
+        custom_icon_id=None):
+    '''
+    Add a <rich_text> subnode with content to the given parent node
+    returns the new rich text node
+    '''
+    rich_text_node = ET.SubElement(node, 'node')     # make the node
+    rich_text_node.set('name', name)
+    rich_text_node.set('prog_lang', prog_lang)
+
+    if custom_icon_id:
+        rich_text_node.set('custom_icon_id', custom_icon_id)
+
+    rich_text = ET.SubElement(rich_text_node, 'rich_text')
+    rich_text.text = str(content) if content else ''  # set the inner text
+    return rich_text_node, rich_text  # return the new rich text node
 
 
 def create_blank_cherrytree_nmap_root():
-	'''
-	Returns a tuple containing the root of the new tree and the blank scan results node
-	'''
-	cherrytree = ET.Element('cherrytree')
-	scan_results = ET.SubElement(cherrytree,'node',attrib={'prog_lang':"custom-colors",'name':"Scan Result"})
-	return (cherrytree, scan_results)
+    '''
+    Returns a tuple containing the root of the new tree 
+    and the blank scan results node
+    '''
+    cherrytree = ET.Element('cherrytree')
+    tree = ET.ElementTree(cherrytree)
+    scan_results = ET.SubElement(cherrytree, 'node', attrib={
+        'prog_lang': "custom-colors",
+        'name': "Scan Result"
+    })
+    return (tree, scan_results)
+
 
 def extract_scan_type_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('scaninfo').get('type')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('scaninfo').get('type')
+    except:
+        return None
+
 
 def extract_scan_protocol_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('scaninfo').get('protocol')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('scaninfo').get('protocol')
+    except:
+        return None
+
 
 def extract_scan_num_services_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('scaninfo').get('numservices')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('scaninfo').get('numservices')
+    except:
+        return None
+
 
 def extract_scan_services_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('scaninfo').get('services')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('scaninfo').get('services')
+    except:
+        return None
+
 
 def extract_scan_start_time_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.getroot().get('startstr')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.getroot().get('startstr')
+    except:
+        return None
+
 
 def extract_scan_end_time_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('runstats').find('finished').get('timestr')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('runstats').find('finished').get('timestr')
+    except:
+        return None
+
 
 def extract_scan_args_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.getroot().get('args')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.getroot().get('args')
+    except:
+        return None
+
 
 def extract_scan_summary_from_raw_nmap_tree(raw_nmap_tree):
-	try:
-		return raw_nmap_tree.find('runstats').find('finished').get('summary')
-	except:
-		return None
+    try:
+        return raw_nmap_tree.find('runstats').find('finished').get('summary')
+    except:
+        return None
+
 
 def extract_scan_metadata_from_raw_nmap_tree(raw_nmap_tree):
-	return {
-		'type':			extract_scan_type_from_raw_nmap_tree(raw_nmap_tree),
-		'protocol':		extract_scan_protocol_from_raw_nmap_tree(raw_nmap_tree),
-		'num_services': extract_scan_num_services_from_raw_nmap_tree(raw_nmap_tree),
-		'services':		extract_scan_services_from_raw_nmap_tree(raw_nmap_tree),
-		'start_time':	extract_scan_start_time_from_raw_nmap_tree(raw_nmap_tree),
-		'end_time':		extract_scan_end_time_from_raw_nmap_tree(raw_nmap_tree),
-		'args':			extract_scan_args_from_raw_nmap_tree(raw_nmap_tree),
-		'summary':		extract_scan_summary_from_raw_nmap_tree(raw_nmap_tree)
-	}
+    return {
+        'type': extract_scan_type_from_raw_nmap_tree(raw_nmap_tree),
+        'protocol': extract_scan_protocol_from_raw_nmap_tree(raw_nmap_tree),
+        'num_services': extract_scan_num_services_from_raw_nmap_tree(raw_nmap_tree),
+        'services': extract_scan_services_from_raw_nmap_tree(raw_nmap_tree),
+        'start_time': extract_scan_start_time_from_raw_nmap_tree(raw_nmap_tree),
+        'end_time': extract_scan_end_time_from_raw_nmap_tree(raw_nmap_tree),
+        'args': extract_scan_args_from_raw_nmap_tree(raw_nmap_tree),
+        'summary': extract_scan_summary_from_raw_nmap_tree(raw_nmap_tree)
+    }
 
 
 def extract_hosts_from_raw_nmap_tree(raw_nmap_tree):
-	return raw_nmap_tree.findall('host')
+    try:
+        return raw_nmap_tree.findall('host')
+    except:
+        return None
+
 
 def extract_ip_addr_from_host(host):
-	try:
-		return host.find("address[@addrtype='ipv4']").get('addr')
-	except:
-		return None
+    try:
+        return host.find("address[@addrtype='ipv4']").get('addr')
+    except:
+        return None
+
 
 def extract_mac_addr_from_host(host):
-	try:
-		return host.find("address[@addrtype='mac']").get('addr')
-	except:
-		return None
+    try:
+        return host.find("address[@addrtype='mac']").get('addr')
+    except:
+        return None
+
 
 def extract_up_reason_from_host(host):
-	try:
-		return host.find("status").get('reason')
-	except:
-		return None
+    try:
+        return host.find("status").get('reason')
+    except:
+        return None
+
 
 def extract_hostname_from_host(host):
-	try:
-		return host.find("hostnames").find('hostname').text
-	except:
-		return None
+    try:
+        return host.find("hostnames").find('hostname').text
+    except:
+        return None
+
 
 def extract_host_metadata_from_host(host):
-		return {
-			'ip_addr' :	extract_ip_addr_from_host(host),
-		#	'mac_addr':	extract_mac_addr_from_host(host),
-			'up_reason': extract_up_reason_from_host(host),
-		#	'hostnames': extract_hostname_from_host(host)
-		}
-
-def run():
-	'''
-	When invoked on the CLI, do the following:
-	1. get scan metadata from nmap.xml
-	2. get hosts and for each host
-	3. get ports and the respective services, and script output
-	4. format extracted data into a cherrytree
-	5. write the resulting cherrytree to a file
-	'''
-
-	# 1. get scan metadata from nmap.xml
-	raw_nmap_tree = create_raw_nmap_output_root_from_file(filename="test_nmap_output.xml")
+    return {
+        'ip_addr' :     extract_ip_addr_from_host(host),
+        'up_reason': extract_up_reason_from_host(host),
+    }
 
 
-	# 2. get hosts and for each host
-	# 3. get ports and the respective services, and script output
-	# 4. format extracted data into a cherrytree
-	# 5. write the resulting cherrytree to a file
-	nmap_root = create_blank_cherrytree_nmap_root()
-	scan_metadata = extract_scan_metadata_from_raw_nmap_tree(raw_nmap_tree)
-	hosts = get_hosts()
-	for host in hosts:
-		host_metadata = extract_host_metadata_from_host(host)
-	#	 ports = # get state @state=open
-		# for each port: 
-			# if state/@state=='open'
-				# <node prog_lang="custom-colors" custom_icon_id="1" name="{portid PROTOCOL i.e. 80 TCP'}">
-			# if state/@state=='filtered'
-				# <node prog_lang="custom-colors" custom_icon_id="2" name="{$portid $protocol.toUpper)}">
-			# state = value of state/@state + ( + state/@reason + )
-			# service = Name + (method), Product + version, extrainfo, cpe.text <service name="http" product="Microsoft IIS httpd" version="10.0" ostype="Windows" method="probed" conf="10">
-			# script = ID > Output: <script id="fingerprint-strings" output="&#xa;	DNSVersionBindReqTCP: &#xa;    version&#xa;    bind">
-	#						 <node prog_lang="custom-colors" name="{@id}">
-	#							 <rich_text>
-	#								 <xsl:value-of select="@output" />
-	#							 </rich_text>
-	#						 </node>
-	#						 <xsl:if test="service">
-	#							 <rich_text>
+def extract_ports_from_host(host):
+    try:
+        return host.find('ports').findall('port')
+    except:
+        return None
+
+
+def extract_portid_from_port(port):
+    try:
+        return port.get('portid')
+    except:
+        return None
+
+
+def extract_protocol_from_port(port):
+    try:
+        return port.get('protocol')
+    except:
+        return None
+
+
+def extract_state_from_port(port):
+    try:
+        return port.find('state').get('state')
+    except:
+        return None
+
+
+def extract_service_from_port(port):
+    try:
+        return port.find('service').get('product')
+    except:
+        return None
+
+
+def extract_port_metadata_from_port(port):
+    return {
+        'portid': extract_portid_from_port(port),
+        'protocol': extract_protocol_from_port(port),
+        'state': extract_state_from_port(port),
+        'service': extract_service_from_port(port),
+    }
+
+
+def extract_scripts_from_port(port):
+    try:
+        return port.findall('script')
+    except:
+        return None
+
+
+def extract_id_from_script(script):
+    try:
+        return script.get('id')
+    except:
+        return None
+
+
+def extract_output_from_script(script):
+    try:
+        return script.get('output')
+    except:
+        return None
+
+
+def extract_script_metadata_from_script(script):
+    return {
+        'id': extract_id_from_script(script),
+        'output': extract_output_from_script(script)
+    }
+
+
+def run(filename):
+    '''
+    When invoked on the CLI, do the following:
+        1. get scan metadata from nmap.xml
+        2. get hosts and for each host
+        3. get ports and the respective services, and script output
+        4. format extracted data into a cherrytree
+        5. write the resulting cherrytree to a file
+    '''
+    (cherrytree, nmap_node) = create_blank_cherrytree_nmap_root()
+
+    # 1. get scan metadata from nmap.xml
+    raw_nmap_tree = create_raw_nmap_output_root_from_file(filename=filename)
+    add_rich_text_node_to_node(nmap_node, name='Metadata', content=extract_scan_metadata_from_raw_nmap_tree(raw_nmap_tree))
+
+    # 2. build a cherrytree from all information about the hosts that we care about
+    hosts = extract_hosts_from_raw_nmap_tree(raw_nmap_tree)
+    (hosts_node, leaf) = add_rich_text_node_to_node(nmap_node, name='Hosts')
+    for host in hosts: 
+        # each host's metadata
+        host_metadata = extract_host_metadata_from_host(host)
+        (host_node, leaf) = add_rich_text_node_to_node(hosts_node, name='Host: '+host_metadata['ip_addr'])
+
+        ports = extract_ports_from_host(host)
+        (ports_node, leaf) = add_rich_text_node_to_node(host_node, name='Ports')
+        for port in ports:
+            # each port's metadata
+            port_metadata = extract_port_metadata_from_port(port)
+            custom_icon_id = '2' if port_metadata['state'] == 'open' else '1'
+            (port_node, leaf) = add_rich_text_node_to_node(ports_node, custom_icon_id = custom_icon_id, name='%s %s' % (port_metadata['portid'],port_metadata['protocol']))
+            scripts = extract_scripts_from_port(port)
+            (scripts_node, leaf) = add_rich_text_node_to_node(port_node, name='Scripts')
+            for script in scripts:
+                # each script's metadata
+                script_metadata = extract_script_metadata_from_script(script)
+                (script_node, leaf) = add_rich_text_node_to_node(scripts_node,content = script_metadata['output'], name='%s' % (script_metadata['id']))
+
+        cherrytree.write('new_cherrytree.ctd')
+
 
 if __name__ == "__main__":
-	run()
+    run(sys.argv[1])
--- nmap2cherry.test.py
+++ nmap2cherry.test.py
@@ -2,115 +2,174 @@ import unittest
 import xml.etree.ElementTree as ET
 
 import nmap2cherry as test_subject
-						
+
 
 class TestNmap2Cherry(unittest.TestCase):
 
-	def test_create_raw_nmap_output_root_from_file(self):
-		# 1. setup
-		# specify filename 
-		filename = "test_nmap_output.xml"
+    def test_create_raw_nmap_output_root_from_file(self):
+        # 1. setup
+        # specify filename 
+        filename = "test_nmap_output.xml"
 
-		# 2. execute
-		nmap_output_tree = test_subject.create_raw_nmap_output_root_from_file(filename)
+        # 2. execute
+        nmap_output_tree = test_subject.create_raw_nmap_output_root_from_file(filename)
 
-		# 3. assert
-		self.assertEquals(nmap_output_tree.tag, "nmaprun", msg="Error: test XML not loaded as expected")
-
-
-
-	def test_add_rich_text_node(self):
-		# 1. setup
-		# create parent node
-		parent_node = ET.Element('parent_node')
-
-		# set content string
-		content_string = "content string for rich text"
-
-		# 2. exectue
-		new_rich_text_node = test_subject.add_rich_text_to_node(parent_node, content_string)
-
-		# 3. assert 
-		self.assertIn(new_rich_text_node, parent_node, msg = "Error: wrong parent on rich text node")
-		self.assertEquals(new_rich_text_node.text, content_string, msg = "Error: wrong content in rich text node")
-
-
-	def test_create_blank_cherrytree_nmap_root(self):
-		# 1. setup
-		# none
-
-		# 2. execute
-		(cherrytree, results) = test_subject.create_blank_cherrytree_nmap_root()
-
-		# 3. assert
-		self.assertEquals(cherrytree.tag, "cherrytree", msg = "Error: new nmap node must be nested in <cherrytree>")
-
-		child_count = 0
-		for child in cherrytree:
-			child_count += 1
-			self.assertEquals(child.tag, "node", msg = "Error: new nmap xml entity should be <node>")
-			self.assertEquals(child.get('name'), "Scan Result", msg = "Error: new nmap node missing expected name attribute")
-			self.assertEquals(child.get('prog_lang'), "custom-colors", msg = "Error: new nmap node missing expected prog_lang attribute")
-
-		self.assertEquals(child_count, 1, msg = "Error: only <cherrytree><node name='Scan Results' ...></...>")
-
-
-	def test_parse_scan_metadata(self):
-		# 1. setup
-		# load in test XML file
-		tree = ET.ElementTree(file="test_nmap_output.xml")
-
-		# 2. execute
-		scan_metadata = test_subject.extract_scan_metadata_from_raw_nmap_tree(tree)
-
-		# 3. assert
-		self.assertEquals(scan_metadata['type'], 'syn', msg = "Error: scan metadata failed to successfully parse type")
-		self.assertEquals(scan_metadata['protocol'], 'tcp', msg = "Error: scan metadata failed to successfully parse protocol")
-		self.assertEquals(scan_metadata['num_services'], '65536', msg = "Error: scan metadata failed to successfully parse num_services")
-		self.assertEquals(scan_metadata['services'], '0-65535', msg = "Error: scan metadata failed to successfully parse services")
-		self.assertEquals(scan_metadata['start_time'], 'Sun Aug 16 14:30:14 2020', msg = "Error: scan metadata failed to successfully parse start_time")
-		self.assertEquals(scan_metadata['end_time'], 'Sun Aug 16 15:03:26 2020', msg = "Error: scan metadata failed to successfully parse end_time")
-		self.assertEquals(scan_metadata['args'], 'nmap -p0- -v -A -T4 -oN nmap_output.nmap -oG nmap_output.grep -oX nmap_output.xml 10.10.10.179,180,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,203', msg = "Error: scan metadata failed to successfully parse args")
-		self.assertEquals(scan_metadata['summary'], 'Nmap done at Sun Aug 16 15:03:26 2020; 20 IP addresses (20 hosts up) scanned in 1992.83 seconds', msg = "Error: scan metadata failed to successfully parse summary")
-
-
-	def test_get_hosts(self):
-		# 1. setup
-		# load in test xml file
-		tree = ET.ElementTree(file="test_nmap_output.xml")
-
-		# 2. execute
-		hosts = test_subject.extract_hosts_from_raw_nmap_tree(tree)
-
-		# 3. assert
-		self.assertEquals(len(hosts), 20, msg = "Error: Incorrect number of hosts")
-	
-	def test_get_host_metadata(self):
-		# 1. setup
-		# pull a host from the tree
-		host = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0]
-
-		# 2. execute
-		host_metadata = test_subject.extract_host_metadata_from_host(host)
-
-		# 3. assert
-		self.assertEquals(host_metadata['ip_addr'], '10.10.10.179', msg = "Error: host metadata failed to successfully parse ip_addr")
-	#	self.assertEquals(host_metadata['mac_addr'], None, msg = "Error: host metadata failed to successfully parse mac_addr")
-		self.assertEquals(host_metadata['up_reason'], 'echo-reply', msg = "Error: host metadata failed to successfully parse up_reason")
-	#	self.assertEquals(host_metadata['hostnames'], None, msg = "Error: host metadata failed to successfully parse hostnames")
+        # 3. assert
+        self.assertEquals(nmap_output_tree.tag, "nmaprun", msg="Error: test XML not loaded as expected")
 
 
 
-#	def test_(self):
-#		# 1. setup
-#		# 
+    def test_add_rich_text_node_to_node(self):
+        # 1. setup
+        # create parent node
+        parent_node = ET.Element('parent_node')
+
+        # set icon id
+        icon_id = 1
+
+        # set name
+        name = "name"
+
+        # set content string
+        content_string = "content string for rich text"
+
+        # 2. exectue
+        (new_rich_text_node, new_rich_text) = test_subject.add_rich_text_node_to_node(parent_node, content = content_string, custom_icon_id = icon_id, name = name)
+
+        # 3. assert 
+        self.assertIn(new_rich_text_node, parent_node, msg = "Error: wrong parent on rich text node")
+        self.assertEquals(new_rich_text_node.tag, 'node', msg = 'Error: wrong tag type on rich text node')
+        self.assertEquals(new_rich_text_node.get('prog_lang'), 'custom_colors', msg = 'Error: wrong prog_lang on rich text node')
+        self.assertEquals(new_rich_text_node.get('custom_icon_id'), icon_id, msg = 'Error: wrong icon id on rich text node')
+        self.assertEquals(new_rich_text_node.get('name'), name, msg = 'Error: wrong icon id on rich text node')
+
+        self.assertEquals(new_rich_text.tag, 'rich_text', msg = 'Error: wrong tag type on rich text')
+        self.assertIn(new_rich_text, new_rich_text_node, msg = "Error: wrong parent on rich text")
+        self.assertEquals(new_rich_text.text, content_string, msg = "Error: wrong content in rich text")
+
+
+    def test_create_blank_cherrytree_nmap_root(self):
+        # 1. setup
+        # none
+
+        # 2. execute
+        (cherrytree, results) = test_subject.create_blank_cherrytree_nmap_root()
+
+        # 3. assert
+        self.assertEquals(cherrytree.getroot().tag, "cherrytree", msg = "Error: new nmap node must be nested in <cherrytree>")
+
+        child_count = 0
+        for child in cherrytree.getroot():
+            child_count += 1
+            self.assertEquals(child.tag, "node", msg = "Error: new nmap xml entity should be <node>")
+            self.assertEquals(child.get('name'), "Scan Result", msg = "Error: new nmap node missing expected name attribute")
+            self.assertEquals(child.get('prog_lang'), "custom-colors", msg = "Error: new nmap node missing expected prog_lang attribute")
+
+            self.assertEquals(child_count, 1, msg = "Error: only <cherrytree><node name='Scan Results' ...></...>")
+
+
+    def test_parse_scan_metadata(self):
+        # 1. setup
+        # load in test XML file
+        tree = ET.ElementTree(file="test_nmap_output.xml")
+
+        # 2. execute
+        scan_metadata = test_subject.extract_scan_metadata_from_raw_nmap_tree(tree)
+
+        # 3. assert
+        self.assertEquals(scan_metadata['type'], 'syn', msg = "Error: scan metadata failed to successfully parse type")
+        self.assertEquals(scan_metadata['protocol'], 'tcp', msg = "Error: scan metadata failed to successfully parse protocol")
+        self.assertEquals(scan_metadata['num_services'], '65536', msg = "Error: scan metadata failed to successfully parse num_services")
+        self.assertEquals(scan_metadata['services'], '0-65535', msg = "Error: scan metadata failed to successfully parse services")
+        self.assertEquals(scan_metadata['start_time'], 'Sun Aug 16 14:30:14 2020', msg = "Error: scan metadata failed to successfully parse start_time")
+        self.assertEquals(scan_metadata['end_time'], 'Sun Aug 16 15:03:26 2020', msg = "Error: scan metadata failed to successfully parse end_time")
+        self.assertEquals(scan_metadata['args'], 'nmap -p0- -v -A -T4 -oN nmap_output.nmap -oG nmap_output.grep -oX nmap_output.xml 10.10.10.179,180,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,203', msg = "Error: scan metadata failed to successfully parse args")
+        self.assertEquals(scan_metadata['summary'], 'Nmap done at Sun Aug 16 15:03:26 2020; 20 IP addresses (20 hosts up) scanned in 1992.83 seconds', msg = "Error: scan metadata failed to successfully parse summary")
+
+
+    def test_get_hosts(self):
+        # 1. setup
+        # load in test xml file
+        tree = ET.ElementTree(file="test_nmap_output.xml")
+
+        # 2. execute
+        hosts = test_subject.extract_hosts_from_raw_nmap_tree(tree)
+
+        # 3. assert
+        self.assertEquals(len(hosts), 20, msg = "Error: Incorrect number of hosts")
+
+    def test_extract_host_metadata(self):
+        # 1. setup
+        # pull a host from the tree
+        host = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0]
+
+        # 2. execute
+        host_metadata = test_subject.extract_host_metadata_from_host(host)
+
+        # 3. assert
+        self.assertEquals(host_metadata['ip_addr'], '10.10.10.179', msg = "Error: host metadata failed to successfully parse ip_addr")
+        #self.assertEquals(host_metadata['mac_addr'], None, msg = "Error: host metadata failed to successfully parse mac_addr")
+        self.assertEquals(host_metadata['up_reason'], 'echo-reply', msg = "Error: host metadata failed to successfully parse up_reason")
+        #self.assertEquals(host_metadata['hostnames'], None, msg = "Error: host metadata failed to successfully parse hostnames")
+
+    def test_extract_ports_from_host(self):
+        # 1. setup
+        # pull a host from the tree
+        host = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0]
+
+        # 2. execute
+        ports = test_subject.extract_ports_from_host(host)
+
+        # 3. assert
+        self.assertEquals(len(ports), 22, msg = "Error: unexpected number of extracted ports")
+
+    def test_extract_scripts_from_ports(self):
+        # 1. setup
+        # pull a port from the tree
+        port = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0].find('ports').findall('port')[1]
+
+        # 2. execute
+        scripts = test_subject.extract_scripts_from_port(port)
+
+        # 3. assert
+        self.assertEquals(len(scripts), 4, msg = "Error: unexpected number of extracted scripts")
+
+    def test_extract_script_metadata(self):
+        # 1. setup
+        # pull a script from the tree
+        script = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0].find('ports').findall('port')[1].findall('script')[0]
+
+        # 2. execute
+        script_metadata = test_subject.extract_script_metadata_from_script(script)
+
+        # 3. assert
+        self.assertEquals(script_metadata['id'], 'http-favicon', msg = "Error: host metadata failed to successfully parse id")
+        self.assertEquals(script_metadata['output'], 'Unknown favicon MD5: 6944F7C42798BE78E1465F1C49B5BF04', msg = "Error: host metadata failed to successfully parse output")
+
+    def test_extract_port_metadata(self):
+        # 1. setup
+        # pull a port from the tree
+        port = ET.ElementTree(file="test_nmap_output.xml").findall('host')[0].find('ports').findall('port')[1]
+
+        # 2. execute
+        port_metadata = test_subject.extract_port_metadata_from_port(port)
+
+        # 3. assert
+        self.assertEquals(port_metadata['portid'], '80', msg = "Error: host metadata failed to successfully parse portid")
+        self.assertEquals(port_metadata['protocol'], 'tcp', msg = "Error: host metadata failed to successfully parse protocol")
+        self.assertEquals(port_metadata['state'], 'open', msg = "Error: host metadata failed to successfully parse state")
+        self.assertEquals(port_metadata['service'], 'Microsoft IIS httpd', msg = "Error: host metadata failed to successfully parse service")
+
+#       def test_(self):
+#               # 1. setup
+#               # 
 #
-#		# 2. execute
-#		pass
+#               # 2. execute
+#               pass
 #
-#		# 3. assert
-#		self.assertEquals(, msg = "Error: ")
+#               # 3. assert
+#               self.assertEquals(, msg = "Error: ")
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
